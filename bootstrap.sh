ea21a6b3a7cf32da00a70c89571ad3d20f9baff0
jp-belanger <8102483+jp-belanger@users.noreply.github.com>
2025-12-31 17:08:45 -0500

    fix: make bootstrap executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 1. Update and install Ansible
+# Update and install Ansible
 sudo pacman -Syu --noconfirm ansible
 
-# 2. Install Yay
+# Install Yay
 if ! command -v yay &> /dev/null; then
     echo "Installing yay..."
     git clone https://aur.archlinux.org/yay-bin.git
@@ -13,5 +13,8 @@ if ! command -v yay &> /dev/null; then
     rm -rf yay-bin
 fi
 
-# 3. Run the Playbook
+# Install ansible aur helper module
+ansible-galaxy collection install kewlfft.aur
+
+# Run the Playbook
 # ansible-playbook local.yml --ask-become-pass
